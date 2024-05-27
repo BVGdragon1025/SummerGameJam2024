dcd0071c459f4bd3c2d689f1b20cd5c340565353
Dominik Nawrot <dnawrot1025@gmail.com>
2024-05-27 15:00:54 +0200

    Movement fix and very basic building
    - Fixed floatines in movement
    - Added VERY basic building placement

---
+++ Assets/Scripts/Buildings/Core/Building.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Building : MonoBehaviour
+{
+    [SerializeField]
+    protected int buildingCost = 0;
+    
+}
--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -2,35 +2,56 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(BuildingManager))]
+
 public class PlayerController : MonoBehaviour
 {
     [SerializeField]
     private float _playerSpeed;
     private Rigidbody2D _rb;
+    [SerializeField]
+    private BuildingManager _buildingManager;
     private Vector2 _movement;
+    [SerializeField]
+    private GameObject _currentObjectToBuild;
+    public Collider2D playerRange;
 
     private void Awake()
     {
         _rb = GetComponent<Rigidbody2D>();
+        _buildingManager = GetComponent<BuildingManager>();
+        playerRange = GetComponentInChildren<Collider2D>();
     }
 
-    private void FixedUpdate()
-    {
-        _rb.velocity = _movement * _playerSpeed;
-    }
-
-    // Update is called once per frame
     void Update()
     {
-        float hInput = Input.GetAxis("Horizontal");
-        float vInput = Input.GetAxis("Vertical");
+        float hInput = Input.GetAxisRaw("Horizontal");
+        float vInput = Input.GetAxisRaw("Vertical");
 
         _movement = new Vector2(hInput, vInput).normalized;
+        Debug.Log($"Velocity: { _movement }");
 
         //Test SFX
         if (Input.GetKeyDown(KeyCode.Space))
         {
             AudioManager.Instance.PlayOneShot(FMODEvents.Instance.testSFXEvent, transform.position);
         }
+
+        _buildingManager.BuildStructure(_currentObjectToBuild);
+
     }
+    
+    private void FixedUpdate()
+    {
+        MovePlayer(_movement, _playerSpeed);
+    }
+
+
+    void MovePlayer(Vector2 movementVector, float playerSpeed)
+    {
+        _rb.velocity = movementVector * playerSpeed;
+    }
+
+
+
 }
