e2fc5380a17c509208e1c088efe62ed1385006e4
ElektrycznyGutek <gustawfilmowka@gmail.com>
2024-06-07 19:35:44 +0200

    Tooltip Change

--- Assets/Scripts/ToolTipManager/ToolTipManager.cs
+++ Assets/Scripts/ToolTipManager/ToolTipManager.cs
@@ -1,13 +1,27 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using TMPro;
 
 public class ToolTipManager : MonoBehaviour
 {
     public static ToolTipManager Instance;
 
-    public TextMeshProUGUI textComponent;
+    public TextMeshProUGUI name;
+    public TextMeshProUGUI cost;
+    public TextMeshProUGUI gives;
+    public TextMeshProUGUI time;
+    public TextMeshProUGUI givesUpgrade;
+    public TextMeshProUGUI addInfo;
+
+    public Image givesImage;
+
+    public Sprite spriteMoney;
+    public Sprite spritePlayerPlague;
+    public Sprite spriteForestPlague;
+
+    public GameObject basic;
 
     private void Awake()
 
@@ -41,25 +55,55 @@ public class ToolTipManager : MonoBehaviour
         switch (building.BuildingType)
         {
             case BuildingType.ResourceUpgrade:
-                textComponent.text = $" Name: {building.BuildingName}, \n Cost: {building.BuildingCost}, \n Gives: +{building.Currency} Resource Production, to all structures around.";
+                name.text = $"{building.BuildingName}";
+                cost.text = $"{building.BuildingCost}";
+                basic.SetActive(false);
+                givesUpgrade.text = $"+{building.Currency} Resource Production, to all structures around";
                 break;
             case BuildingType.SpeedUpgrade:
-                textComponent.text = $" Name: {building.BuildingName}, \n Cost: {building.BuildingCost}, \n Gives: {building.Currency * 100}% to Resource Speed, to all structures around.";
+                name.text = $"{building.BuildingName}";
+                cost.text = $"{building.BuildingCost}";
+                basic.SetActive(false);
+                givesUpgrade.text = $"{building.Currency * 100}% to Resource Speed, to all structures around";
                 break;
             case BuildingType.HealthUpgrade:
-                textComponent.text = $" Name: {building.BuildingName}, \n Cost: {building.BuildingCost}, \n Gives: {building.Currency}s to Plague Timer, to all structures around.";
+                name.text = $"{building.BuildingName}";
+                cost.text = $"{building.BuildingCost}";
+                basic.SetActive(false);
+                givesUpgrade.text = $"{building.Currency}s to Plague Timer, to all structures around";
                 break;
             case BuildingType.Tree:
-                textComponent.text = $" Name: {building.BuildingName}, \n Cost: {building.BuildingCost}, \n Gives: {building.Currency} Nature Points / {building.SpawnRate}s, \n Requires element: {building.BuildingType}";
+                name.text = $"{building.BuildingName}";
+                cost.text = $"{building.BuildingCost}";
+                gives.text = $"+{building.Currency}";
+                time.text = $"/{building.SpawnRate}s";
+                addInfo.text = $"Requires {building.BuildingType}";
+                givesImage.sprite = spriteMoney;
+                givesUpgrade.text = string.Empty;
+                basic.SetActive(true);
                 break;
             case BuildingType.Meadow:
-                textComponent.text = $" Name: {building.BuildingName}, \n Cost: {building.BuildingCost}, \n Gives: -{building.Currency} Player Plague / {building.SpawnRate}s, \n Requires element: {building.BuildingType}";
+                name.text = $"{building.BuildingName}";
+                cost.text = $"{building.BuildingCost}";
+                gives.text = $"-{building.Currency}";
+                time.text = $"/{building.SpawnRate}s";
+                addInfo.text = $"Requires {building.BuildingType}";
+                givesImage.sprite = spritePlayerPlague;
+                givesUpgrade.text = string.Empty;
+                basic.SetActive(true);
                 break;
             case BuildingType.Pond:
-                textComponent.text = $" Name: {building.BuildingName}, \n Cost: {building.BuildingCost}, \n Gives: -{building.Currency} Forest Plague / {building.SpawnRate}s, \n Requires element: {building.BuildingType}";
+                name.text = $"{building.BuildingName}";
+                cost.text = $"{building.BuildingCost}";
+                gives.text = $"-{building.Currency}";
+                time.text = $"/{building.SpawnRate}s";
+                addInfo.text = $"Requires {building.BuildingType}";
+                givesImage.sprite = spriteForestPlague;
+                givesUpgrade.text = string.Empty;
+                basic.SetActive(true);
                 break;
             default:
-                textComponent.text = $"Holy Shit, is that an Easter Egg?";
+                name.text = $"Holy Shit, is that an Easter Egg?";
                 break;
         }
 
@@ -67,7 +111,6 @@ public class ToolTipManager : MonoBehaviour
     public void HideToolTip()
     {
         gameObject.SetActive(false);
-        textComponent.text = string.Empty;
     }
 
     public void ShowCursor()
