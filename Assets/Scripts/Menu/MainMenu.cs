4d2ec8ca46c02bc3df1003e438fd50dce658ba63
Dominik Nawrot <dnawrot1025@gmail.com>
2024-08-06 14:45:33 +0200

    UI and Video changes
    - Further adjusted player bars
    - Added abillity to skip intro (scenes now load asynchronously)
    
    TODO:
    - Make skip text more visible

--- Assets/Scripts/Menu/MainMenu.cs
+++ Assets/Scripts/Menu/MainMenu.cs
@@ -2,11 +2,13 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.Video;
 
 public class MainMenu : MonoBehaviour
 {
 	[SerializeField] private ScenesData _scenesData;
 	[SerializeField] private GameObject _resetManager;
+	AsyncOperation _asyncSceneLoad = null;
 
 	public void Play()
 	{
@@ -17,6 +19,54 @@ public class MainMenu : MonoBehaviour
 		SceneManager.LoadScene(GetRandomScene());
 		_scenesData.scenesPlayed++;
 	}
+
+	public void PlayAsync(GameObject skipButton, VideoPlayer videoPlayer)
+	{
+        if (Time.timeScale <= 0)
+        {
+            Time.timeScale = 1;
+        }
+
+		StartCoroutine(ShowSkipButton(skipButton, videoPlayer));
+
+    }
+
+	IEnumerator ShowSkipButton(GameObject skipButtonObject, VideoPlayer videoPlayer)
+	{
+		yield return null;
+
+		_asyncSceneLoad = SceneManager.LoadSceneAsync(GetRandomScene());
+		_asyncSceneLoad.allowSceneActivation = false;
+
+
+		while (!_asyncSceneLoad.isDone)
+		{
+            if (_asyncSceneLoad.progress >= 0.9f)
+            {
+				if(videoPlayer.isPlaying)
+					skipButtonObject.SetActive(true);
+
+                if (Input.GetKeyDown(KeyCode.Space) && _asyncSceneLoad.allowSceneActivation != true)
+                {
+                    skipButtonObject.SetActive(false);
+                    videoPlayer.Stop();
+                    _asyncSceneLoad.allowSceneActivation = true;
+                    _scenesData.scenesPlayed++;
+                }
+            }
+
+			yield return null;
+
+        }
+
+    }
+
+	public void PlayNextSceneAsync()
+	{
+		_asyncSceneLoad.allowSceneActivation = true;
+		_scenesData.scenesPlayed++;
+	}
+
 	public void GoBackToMenu()
 	{
 		Time.timeScale = 1f;
@@ -42,6 +92,7 @@ public class MainMenu : MonoBehaviour
 		_resetManager.SetActive(true);
 
 		SceneManager.LoadScene(GetRandomScene());
+		_scenesData.scenesPlayed++;
     }
 
 	public void StopMusicInMenu()
